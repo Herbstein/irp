85e571f4961f326d96f09b5e22557f6f1c2ba2a8
Rasmus Larsen <doctorherbstein@gmail.com>
2026-04-03 18:17:58 +0200

    Only generate the irp-proto server and client implementations when needed

--- irp-proto/build.rs
+++ irp-proto/build.rs
@@ -1,15 +1,7 @@
 fn main() {
-    let mut builder = tonic_prost_build::configure();
-
-    #[cfg(feature = "server")]
-    {
-        builder = builder.build_server(true);
-    }
-
-    #[cfg(feature = "client")]
-    {
-        builder = builder.build_client(true);
-    }
-
-    builder.compile_protos(&["irp.proto"], &["proto/"]).unwrap();
+    tonic_prost_build::configure()
+        .build_server(cfg!(feature = "server"))
+        .build_client(cfg!(feature = "client"))
+        .compile_protos(&["irp.proto"], &["proto/"])
+        .unwrap();
 }
